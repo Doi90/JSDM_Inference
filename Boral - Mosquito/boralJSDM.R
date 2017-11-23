23a40b8143b9d84774c2ef466783f41e007660c7
Doi90 <davidpw@student.unimelb.edu.au>
2017-11-23 16:34:37 +1100

    boral - Mosquito

---
+++ Boral - Mosquito/boralJSDM.R
@@ -0,0 +1,26 @@
+# Load packages
+
+library(boral)
+
+# Read in data
+
+PA <- read.csv("Mosquito_PA.csv")
+
+Env <- read.csv("Mosquito_Covar.csv")
+
+# model
+
+system.time(
+  
+JSDM <- boral(PA,  # PA data
+              X = Env,  # Covariates, DO NOT INCLUDE INTERCEPT COLUMN
+              family = "binomial",  # makes model use PA data, probit link
+              num.lv = 2, # set number of latent variables
+              save.model = TRUE, # saves JAGS model as a txt file, allows coda package to analyse MCMC
+              mcmc.control = list(n.burnin = 10000,  # mcmc set up. using set up from Warton TREE paper
+                                  n.iteration = 60000,
+                                  n.thin = 50,
+                                  seed = 28041948),
+              model.name = NULL)  # name of saved txt file. Can change, but default means dont have to change code between models
+
+)
