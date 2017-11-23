a7a0227b7af9db4fed20828bfd348b998520d62f
Doi90 <davidpw@student.unimelb.edu.au>
2017-11-23 16:16:22 +1100

    BayesComm - Fungi

---
+++ BayesComm - Fungi/Beta-R-extraction-script.R
@@ -0,0 +1,178 @@
+library(tibble)
+library(BayesComm)
+library(ineq)
+
+##############################
+## Set up species and model ##
+##############################
+
+species <- colnames(fauna)
+model <- "BayesComm"
+
+#######################################
+### Standardise posteriors post-hoc ###
+#######################################
+
+dataset_sd <- read.csv("Fungi_sd.csv")   # load in original data sd
+dataset_sd <- dataset_sd[,2]
+dataset_sd <- c(1, dataset_sd) # add intercept
+
+full_standardised <- full
+
+for(i in seq(length(full_standardised$trace$B))){   # species
+  for(j in seq(length(dataset_sd))){                #covariate
+    full_standardised$trace$B[[i]][,j] <- (full_standardised$trace$B[[i]][,j])/dataset_sd[j]
+  }
+}
+
+##############################
+### Create blank dataframe ### not standardised
+##############################
+
+df <- data.frame(coefficient = numeric(0), posterior.mean = numeric(0), lower = numeric(0),
+                 upper = numeric(0), sd = numeric(0), coefVar = numeric(0), qcd = numeric(0),
+                 qcd2 = numeric(0), gini = numeric(0), model = numeric(0), species = numeric(0))
+colnames(df) <- c("coefficient", "posterior.mean", "lower", "upper", "sd", 
+                  "coefVar", "qcd", "qcd2", "gini", "model", "species")
+
+##############################
+## For loop data extraction ## not standardised
+##############################
+
+for(i in 1:length(species)){
+  command <- sprintf('summary(%s, "%s$%s")', 'full', 'B', species[i])
+  tmp <- eval(parse(text = command))
+  sum.full <- tmp
+  stat.full <- sum.full$statistics
+  quan.full <- sum.full$quantiles
+  gini.command <- sprintf('full$trace$B$%s', species[i])
+  gini.eval <- eval(parse(text = gini.command))
+  gini <- numeric(0)
+  for(j in seq(ncol(gini.eval))){
+    tmp <- ineq(gini.eval[,j], type = "Gini")
+    gini <- c(gini, tmp)
+  }
+  dfr <- cbind(c("intercept", colnames(preds)), stat.full[,1], quan.full[,1], quan.full[,5],
+               stat.full[,2], stat.full[,2]/stat.full[,1],
+               (quan.full[,4] - quan.full[, 2]) / (quan.full[,4] + quan.full[, 2]),
+               (quan.full[,5] - quan.full[, 1]) / (quan.full[,5] + quan.full[, 1]),
+               gini, rep(model, ncol(preds)+1), rep(species[i], ncol(preds)+1))
+  colnames(dfr) <- c("coefficient", "posterior.mean", "lower", "upper", "sd", 
+                     "coefVar", "qcd", "qcd2", "gini", "model", "species")
+  dfr <- as.data.frame(dfr)
+  dfr$coefficient <- c("intercept", colnames(preds))
+  df <- rbind(df, dfr)
+}
+
+rownames(df) <- NULL
+head(df)
+
+##############################
+### Create blank dataframe ### standardised
+##############################
+
+df2 <- data.frame(coefficient = numeric(0), posterior.mean = numeric(0), lower = numeric(0),
+                  upper = numeric(0), sd = numeric(0), coefVar = numeric(0), qcd = numeric(0),
+                  qcd2 = numeric(0), gini = numeric(0), model = numeric(0), species = numeric(0))
+colnames(df2) <- c("coefficient", "posterior.mean", "lower", "upper", "sd", 
+                   "coefVar", "qcd", "qcd2", "gini", "model", "species")
+
+##############################
+## For loop data extraction ## standardised
+##############################
+
+for(i in 1:length(species)){
+  command <- sprintf('summary(%s, "%s$%s")', 'full_standardised', 'B', species[i])
+  tmp <- eval(parse(text = command))
+  sum.full <- tmp
+  stat.full <- sum.full$statistics
+  quan.full <- sum.full$quantiles
+  gini.command <- sprintf('full_standardised$trace$B$%s', species[i])
+  gini.eval <- eval(parse(text = gini.command))
+  gini <- numeric(0)
+  for(j in seq(ncol(gini.eval))){
+    tmp <- ineq(gini.eval[,j], type = "Gini")
+    gini <- c(gini, tmp)
+  }
+  dfr <- cbind(c("intercept", colnames(preds)), stat.full[,1], quan.full[,1], quan.full[,5],
+               stat.full[,2], stat.full[,2]/stat.full[,1],
+               (quan.full[,4] - quan.full[, 2]) / (quan.full[,4] + quan.full[, 2]),
+               (quan.full[,5] - quan.full[, 1]) / (quan.full[,5] + quan.full[, 1]),
+               gini, rep(model, ncol(preds)+1), rep(species[i], ncol(preds)+1))
+  colnames(dfr) <- c("coefficient", "posterior.mean", "lower", "upper", "sd", 
+                     "coefVar", "qcd", "qcd2", "gini", "model", "species")
+  dfr <- as.data.frame(dfr)
+  dfr$coefficient <- c("intercept", colnames(preds))
+  df2 <- rbind(df2, dfr)
+}
+
+rownames(df2) <- NULL
+head(df2)
+
+#############################################
+### Combine relevant sections of df & df2 ###
+#############################################
+
+df[,5:9] <- df2[,5:9]
+
+##############################
+#### Extract correlation #####
+##############################
+
+correl.summ <- summary(full, 'R')
+n.species <- ncol(fauna)
+
+Sigma.mean <- matrix(0, nrow = n.species, ncol = n.species)
+Sigma.mean[upper.tri(Sigma.mean)] <- correl.summ$statistics[, 'Mean']
+Sigma.mean <- Sigma.mean + t(Sigma.mean)
+diag(Sigma.mean) <- 1
+rownames(Sigma.mean) <- colnames(fauna)
+colnames(Sigma.mean) <- colnames(fauna)
+
+Sigma.lower <- matrix(0, nrow = n.species, ncol = n.species)
+Sigma.lower[upper.tri(Sigma.lower)] <- correl.summ$quantiles[, "2.5%"]
+Sigma.lower <- Sigma.lower + t(Sigma.lower)
+diag(Sigma.lower) <- 1
+rownames(Sigma.lower) <- colnames(fauna)
+colnames(Sigma.lower) <- colnames(fauna)
+
+Sigma.upper <- matrix(0, nrow = n.species, ncol = n.species)
+Sigma.upper[upper.tri(Sigma.upper)] <- correl.summ$quantiles[, "97.5%"]
+Sigma.upper <- Sigma.upper + t(Sigma.upper)
+diag(Sigma.upper) <- 1
+rownames(Sigma.upper) <- colnames(fauna)
+colnames(Sigma.upper) <- colnames(fauna)
+
+Sigma.sd <- matrix(0, nrow = n.species, ncol = n.species)
+Sigma.sd[upper.tri(Sigma.sd)] <- correl.summ$statistics[, 'SD']
+Sigma.sd <- Sigma.sd + t(Sigma.sd)
+diag(Sigma.sd) <- 1
+rownames(Sigma.sd) <- colnames(fauna)
+colnames(Sigma.sd) <- colnames(fauna)
+
+##############################
+### Effective Sample Size ####
+##############################
+
+ess <- data.frame(a = numeric(0), b = numeric(0), c = numeric(0), d = numeric(0), e = numeric(0),
+                  f = numeric(0), g = numeric(0), h = numeric(0), i = numeric(0), j = numeric(0),
+                  k = numeric(0), l = numeric(0), m = numeric(0), n = numeric(0))
+colnames(ess) <- c("intercept", colnames(preds))
+
+for(i in 1:length(species)){
+  command <- sprintf('effectiveSize(full[1]$trace$B$%s)', species[i])
+  tmp <- eval(parse(text = command))
+  ess <- rbind(ess, tmp)
+  colnames(ess) <- c("intercept", colnames(preds))
+}
+
+##############################
+######### Write csv ##########
+##############################
+
+write.csv(df, "Beta_df_Fungi_BayesComm.csv")
+write.csv(Sigma.mean, "Rho_mean_Fungi_BayesComm.csv")
+write.csv(Sigma.lower, "Rho_lower_Fungi_BayesComm.csv")
+write.csv(Sigma.upper, "Rho_upper_Fungi_BayesComm.csv")
+write.csv(Sigma.sd, "Rho_sd_Fungi_BayesComm.csv")
+write.csv(ess, "ess_Fungi_BayesComm.csv")
---
+++ BayesComm - Fungi/analysis_script.R
@@ -0,0 +1,133 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~
+# 1. housekeeping
+rm(list = ls())
+
+library(BayesComm)  # Bayesian multivariate binomial models
+library(spdep)  # Moran's I test
+library(ncf)  # semivariograms
+library(coda)
+
+# for plotting
+library(igraph)  # networks
+library(gridExtra) # ggplot layouts
+
+#~~~~~~~~~~~~~~~~~~~~~~~~
+# 2. dataset manipulation and diversity indices
+
+# load data
+dat <- read.csv("Fungi_Compiled.csv")
+
+# convert fauna to presence/absence
+fauna <- as.matrix(dat[, 1:11])
+
+# covariates (scale is data set dependant)
+preds <- as.matrix(dat[, 12:23])
+#preds[, 1:4] <- scale(preds[, 1:4]) Don't need to scale these
+
+#~~~~~~~~~~~~~~~~~~~~~~~~
+# # 4. selection of environmental predictors
+# 
+# # create a new dataframe with PC values and round/site factors
+# df <- data.frame(preds, dat$field_site)
+# 
+# # run forward stepwise selection across all covariates for each species
+# # select the model that gives the lowest AIC and tabulate which were selected
+# 
+# #create matrix of results (whether PC was selected)
+# res <- matrix(0, ncol(fauna), ncol(preds))
+# colnames(res) <- colnames(preds)
+# rownames(res) <- colnames(fauna)
+# 
+# # an empty list for the selected models
+# mods <- list()
+# 
+# # run the selection and record results for each species
+# for (i in 1:ncol(fauna)) {
+#   # starting model (forces in round and site)
+#   mlow <- glm(fauna[, i] ~ factor(dat.field_site),
+#               data = df, family = binomial(link = "probit"))
+#   # potential final model (includes all PCs)
+#   mup <- glm(fauna[, i] ~ factor(dat.field_site) + .,
+#              data = df, family = binomial(link = "probit"))
+#   # run model selection
+#   s <- step(mlow, formula(mup), direction = "forward")
+#   # add selected model to the list
+#   mods <- c(mods, list(s))
+#   # get the names of the selected coefficients and update the results matrix
+#   sel <- names(s$coefficients)
+#   ind <- (1:length(sel))[-grep('\\(', sel)]
+#   #  ind <- which(substring(sel, 1, 4) == "Comp")
+#   #   nos <- as.numeric(substring(sel[ind], 6, 7))
+#   res[i, sel[ind]] <- 1
+#   #   res[i, nos] <- 1
+# }
+# 
+# # matrix indicating whether a variable was selected
+# res
+# 
+# # number of times each component was selected for a species:
+# # sort(colSums(res), decreasing = TRUE)
+# 
+# # create a list of the components to use for each species in the modelling
+# covlist <- apply(res, 1, function(x) which(as.logical(x)))
+
+# # ~~~~~~~~~~~~~~~~~~~~~~~~
+# # 5. Bayesian multivariate binomial analysis
+# 
+# # set the number of iterations, how many to discard
+# # and how much to thin the resulting chains
+iterations <- 11000
+burnin = 1000
+thin = 10
+
+# # now run the models!
+# # (warning: the following take several minutes to run
+# # and can be quite memory-intensive, hence the thinning)
+# 
+# # null model (ignores X, but we include it for consistency)
+# system.time(
+# 	null <- BC(Y = fauna, X = preds, model = "null",
+# 		covlist = covlist,
+# 		its = iterations, burn = burnin, thin = thin)
+# 	)
+# 
+# # community model
+# system.time(
+# 	comm <- BC(Y = fauna, X = preds, model = "community",
+# 		covlist = covlist,
+# 		its = iterations, burn = burnin, thin = thin)
+# 	)
+# 
+# # environment model
+# system.time(
+# 	env <- BC(Y = fauna, X = preds, model = "environment",
+#     covlist = covlist,
+# 		its = iterations, burn = burnin, thin = thin)
+# 	)
+# 
+# # full model
+# system.time(
+#   full <- BC(Y = fauna, X = preds, model = "full",
+#              covlist = covlist,
+#              its = iterations, burn = burnin, thin = thin)
+# )
+# 
+system.time(
+  full <- BC(Y = fauna, X = preds, model = "full",
+             its = iterations, burn = burnin, thin = thin)
+)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~
+# 6. deviance partitioning and network plotting
+
+# # deviance partitioning
+# dp <- devpart(null, env, comm, full)
+# dp[[1]]
+
+
+
+## Prediction
+
+preds.new <- preds
+fungi.pred <- predict(full, preds.new)
+
