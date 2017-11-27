ea9a60ed82a8f140bf447baf06aae6a28fb2160c
Doi90 <davidpw@student.unimelb.edu.au>
2017-11-27 14:36:37 +1100

    Ov2016 - Mosquito

---
+++ Ovaskainen 2016 - Mosquito/examples/Mosquito/posteriors/Beta_sd_extraction.R
@@ -0,0 +1,11 @@
+setwd("C:/Users/David/Dropbox/JSDM/JSDM Comparison Compiled/Ovaskainen 2016 - Mosquito/examples/Mosquito/posteriors")
+
+Beta <- read.csv("beta_Mosquito.csv")
+
+sd.vector <- apply(X = Beta, MARGIN = 2, FUN = sd)
+
+# nrow = #spp, ncol = # covar + intercept
+
+sd.matrix <- matrix(sd.vector, nrow = 16, ncol = 14, byrow = T)
+
+write.csv(sd.matrix, "Beta_sd_Mosquito_Ov.csv")
